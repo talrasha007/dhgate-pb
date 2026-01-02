e9819f9fa42c1d59f81d582f598730c1cf565068
talrasha007 <talrasha007@gmail.om>
2026-01-02 09:23:15 +0800

    init sql.

---
+++ init/init.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS pb_order;
+
+CREATE TABLE pb_order (
+    order_num VARCHAR(50) PRIMARY KEY,
+    s_amount DECIMAL(10, 2) NOT NULL,
+    e_commis DECIMAL(10, 2) NOT NULL,
+    c_time TIMESTAMP NOT NULL,
+    p_time TIMESTAMP NOT NULL,
+    region VARCHAR(10),
+    device VARCHAR(20),
+    tracking TEXT,
+    coupon VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_pb_order_p_time ON pb_order(p_time);
+
+DROP TABLE IF EXISTS pb_stats;
+CREATE TABLE pb_stats (
+    hour TIMESTAMP,
+    region VARCHAR(10),
+    total_orders INT NOT NULL,
+    total_amount DECIMAL(15, 2) NOT NULL,
+    total_commission DECIMAL(15, 2) NOT NULL,
+
+    PRIMARY KEY (hour, region)
+);
