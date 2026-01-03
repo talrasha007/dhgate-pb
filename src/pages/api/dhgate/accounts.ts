c5de37954035994127343bd9b02455a948495fef
talrasha007 <talrasha007@gmail.om>
2026-01-03 19:12:46 +0800

    only return account.

--- src/pages/api/dhgate/accounts.ts
+++ src/pages/api/dhgate/accounts.ts
@@ -3,7 +3,7 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = async ({ locals: { runtime: { env: { PB_DB } }} }) => {
   const result = await PB_DB.prepare('SELECT * FROM account').all();
   if (result.success) {
-    return Response.json(result.results);
+    return Response.json(result.results.map(acc => acc.account));
   } else {
     return Response.json({ error: result.error }, { status: 500 });
   }
