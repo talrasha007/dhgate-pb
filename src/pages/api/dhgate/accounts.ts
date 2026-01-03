4c5a78c4a64fe2b88bd73cd61cfec19ef4e9cec2
talrasha007 <talrasha007@gmail.om>
2026-01-03 19:11:18 +0800

    update api return.

--- src/pages/api/dhgate/accounts.ts
+++ src/pages/api/dhgate/accounts.ts
@@ -1,6 +1,10 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ locals: { runtime: { env: { PB_DB } }} }) => {
-  const accounts = await PB_DB.prepare('SELECT * FROM account').all();
-  return Response.json(accounts);
+  const result = await PB_DB.prepare('SELECT * FROM account').all();
+  if (result.success) {
+    return Response.json(result.results);
+  } else {
+    return Response.json({ error: result.error }, { status: 500 });
+  }
 };
