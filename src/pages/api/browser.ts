970e269171c944b127cb715aa01c8831a9686bc7
Tal <talrasha007@gmail.com>
2026-01-24 15:08:50 +0800

    proxy

--- src/pages/api/browser.ts
+++ src/pages/api/browser.ts
@@ -1,13 +1,13 @@
 import puppeteer from '@cloudflare/puppeteer';
-import type { WorkersLaunchOptions } from '@cloudflare/puppeteer';
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ url, locals: { runtime: { env: { MYBROWSER } } } }) => {
   const uri = url.searchParams.get('url') || 'https://d27tmy7atbm8yh.cloudfront.net/';
-  const country = url.searchParams.get('country') || 'US';
+  const proxyServer = url.searchParams.get('proxy') || undefined;
 
-  const browser  = await puppeteer.launch(MYBROWSER, { location: country as WorkersLaunchOptions['location'] });
-  const page = await browser.newPage();
+  const browser  = await puppeteer.launch(MYBROWSER);
+  const context = await browser.createBrowserContext({ proxyServer });
+  const page = await context.newPage();
   await page.goto(uri);
   // const metrics = await page.metrics();
   const html = await page.evaluate(() => document.documentElement.innerHTML);
