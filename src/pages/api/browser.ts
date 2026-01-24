e6a0bce2b05bb820feee125d93742cf23e8fa09a
Tal <talrasha007@gmail.com>
2026-01-24 15:13:04 +0800

    test for proxy.

--- src/pages/api/browser.ts
+++ src/pages/api/browser.ts
@@ -4,10 +4,20 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = async ({ url, locals: { runtime: { env: { MYBROWSER } } } }) => {
   const uri = url.searchParams.get('url') || 'https://d27tmy7atbm8yh.cloudfront.net/';
   const proxyServer = url.searchParams.get('proxy') || undefined;
+  const proxyUsername = url.searchParams.get('proxy-user') || undefined;
+  const proxyPassword = url.searchParams.get('proxy-pass') || undefined;
 
   const browser  = await puppeteer.launch(MYBROWSER);
   const context = await browser.createBrowserContext({ proxyServer });
   const page = await context.newPage();
+
+  if (proxyUsername && proxyPassword) {
+    await page.authenticate({
+      username: proxyUsername,
+      password: proxyPassword,
+    });
+  }
+  
   await page.goto(uri);
   // const metrics = await page.metrics();
   const html = await page.evaluate(() => document.documentElement.innerHTML);
