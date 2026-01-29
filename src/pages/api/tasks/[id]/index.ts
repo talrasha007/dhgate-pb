76850cd5d8975424278ada0a79644e8057dbd274
Tal <talrasha007@gmail.com>
2026-01-29 19:53:43 +0800

    tidy return.

--- src/pages/api/tasks/[id]/index.ts
+++ src/pages/api/tasks/[id]/index.ts
@@ -1,4 +1,5 @@
 import type { APIRoute } from 'astro';
+import { send } from 'node:process';
 
 export const GET: APIRoute = async ({ params, locals: { runtime: { env: { PB_DB } } } }) => {
   const taskSql = 'SELECT * FROM tasks WHERE app_id = ?';
@@ -9,7 +10,13 @@ export const GET: APIRoute = async ({ params, locals: { runtime: { env: { PB_DB 
 
   const itemsSql = 'SELECT * FROM task_items WHERE task_id = ?';
   const taskItems = await PB_DB.prepare(itemsSql).bind(params.id).all();
-  return Response.json({ ...task, countries: JSON.parse(task.countries as string), disabled: !!task.disabled, clicks: taskItems.results.map(item => ({ ...item, disabled: !!item.disabled })) });
+  return Response.json({ ...task,
+    countries: JSON.parse(task.countries as string),
+    send_page_view: !!task.send_page_view,
+    use_page_view: !!task.use_page_view,
+    disabled: !!task.disabled,
+    clicks: taskItems.results.map(item => ({ ...item, disabled: !!item.disabled }))
+});
 };
 
 export const PUT: APIRoute = async ({ params, request, locals: { runtime: { env: { PB_DB } } } }) => {
--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,7 +1,22 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ url, locals: { runtime: { env: { PB_DB } } } }) => {
+  const actived = url.searchParams.get('actived');
+  console.log(actived);
+
   const sql = 'SELECT * FROM tasks';
   const tasks = await PB_DB.prepare(sql).all();
-  return Response.json(tasks.results);
+
+  if (actived === 'true') {
+    const results = tasks.results
+      .filter(task => !task.disabled && task.countries !== '[]')
+      .map(task => {
+        delete task.icon_url;
+        delete task.disabled;
+        return { ...task, countries: JSON.parse(task.countries as string).join(','), disabled: !!task.disabled };
+      });
+    return Response.json(results);
+  } else {
+    return Response.json(tasks.results);
+  }
 }
