ae9aac3479c05fc5ce080774b1ca7197fd6ab02a
Tal <talrasha007@gmail.com>
2026-01-29 16:31:11 +0800

    vue support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
 
     imageService: "cloudflare"
-  })
+  }),
+
+  integrations: [vue()]
 });
